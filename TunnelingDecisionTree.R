1e510db5df9d0c9a099eca6fe01485ce437b3f7d
Irvin Homem <irvinhomem@gmail.com>
2016-10-21 00:38:32 +0200

    Fixed normalization of features to be used for knn with multiple features. Fixed the changing resampling of folds that was happening with the traincontrol function. Now using CreateDataPartition which is creating for stable folds/resamples

--- TunnelingDecisionTree.R
+++ TunnelingDecisionTree.R
@@ -129,9 +129,9 @@ print(pop3_ov_dns_pcap_features_df)
 print(json_features_all_pcaps_df)
 #str(pcap_features_df)
 
-############################
+################################################
 ##  Partition the dataset into k-folds i.e. 10-folds in this case
-############################
+################################################
 library("caret")
 library("rpart")
 
@@ -139,17 +139,59 @@ library("rpart")
 
 #frmla = as.formula(proto_label ~ avg_entropy + avg_ip_req_len + avg_dns_req_len)  # With 3 features
 frmla = as.formula(proto_label ~ avg_entropy + avg_ip_req_len)                   # With 2 features
+#frmla = as.formula(proto_label ~ avg_entropy)                   # With 1 feature
+
 
 #train_ctrl <- trainControl(method = "cv", number = 10, p=0.9, savePredictions = TRUE, returnResamp = TRUE, classProbs = TRUE) # classProbs = TRUE # sampling = "none"
 #train_ctrl <- trainControl(method = "cv", number = 10, p=0.9, savePredictions = "final", returnResamp = TRUE, classProbs = TRUE) # classProbs = TRUE # sampling = "none"
-train_ctrl <- trainControl(method = "cv", number = 10, p=0.9, savePredictions = "final", returnResamp = "final") # classProbs = TRUE # sampling = "none"
+# Best so far, but results in different folds every time a new classifcation algorithm is tried (i.e. decision trees, knn, etc)
+#train_ctrl <- trainControl(method = "cv", number = 10, p=0.9, savePredictions = "final", returnResamp = "final", returnData = TRUE) # classProbs = TRUE # sampling = "none"
+#myFolds <- createFolds(json_features_all_pcaps_df$avg_entropy, k = 10, returnTrain = FALSE)
+#myFolds <- createDataPartition(json_features_all_pcaps_df$avg_entropy, times = 10, p = 0.9, list = FALSE)
+myFolds <- createDataPartition(json_features_all_pcaps_df$proto_label, times = 10, p = 0.9)
+#myFolds
+head(myFolds)
+train_ctrl <- trainControl(method = "cv", index = myFolds, savePredictions = "final", returnResamp = "final", returnData = TRUE) # classProbs = TRUE # sampling = "none"
+#train_ctrl <- trainControl(method = "loocv", index = myFolds, savePredictions = "final", returnResamp = "final", returnData = TRUE)
 
+################################################
+##  Run different Classifiers on the folds
+################################################
 
+#======
+#  Decision Trees
+#======
 #tree_model <- train(frmla, data=json_features_all_pcaps_df, method = "rpart", cp=0.2405063, trControl = train_ctrl)
 #tree_model <- train(frmla, data=json_features_all_pcaps_df, method = "rpart", cp = 0.002, trControl = train_ctrl)
 #tree_model <- train(frmla, data=json_features_all_pcaps_df, method = "rpart", trControl = train_ctrl)   # 'rpart' with 2 features, HTTP-S is missing
 tree_model <- train(frmla, data=json_features_all_pcaps_df, method = "rpart2", trControl = train_ctrl)  # 'rpart2' with 2 features does better classificaiton
 
+#warnings()
+#======
+# k-NN (k-Nearest Neighbours)
+#======
+#"preProcess" in order to normalize with "center" and "scale" (something like z-score-normalization)
+#“center“: subtract mean from values.
+#“scale“: divide values by standard deviation.
+#knnFit <- train(frmla, data=json_features_all_pcaps_df, method = "knn", trControl=train_ctrl, preProcess = c("center", "scale"), tuneLength = 10)
+# 1-30 NN
+knnFit <- train(frmla, data=json_features_all_pcaps_df, method = "knn", trControl=train_ctrl, preProcess = c("center", "scale"), tuneGrid = expand.grid(.k=1:30))
+# only 1-NN
+#knnFit <- train(frmla, data=json_features_all_pcaps_df, method = "knn", trControl=train_ctrl, preProcess = c("center", "scale"), tuneGrid = expand.grid(.k=1)) 1NN
+
+plot(knnFit)
+knnFit
+knnFit$finalModel
+knnFit$results
+knnFit$bestTune
+knnFit$pred[order(knnFit$pred$Resample, knnFit$pred$rowIndex),]
+confusionMatrix(knnFit)
+
+
+
+#########################
+##  Decision Tree results
+#########################
 print(tree_model)
 plot(tree_model)
 summary(tree_model)
